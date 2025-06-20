a34ac01c74d9189fc9a5ad7541f9decd357e8e0d
Brenda Romero <160025540+estufita-programa@users.noreply.github.com>
2025-06-20 20:51:34 -0300

    Animación

---
+++ assets/js/animation.js
@@ -0,0 +1,23 @@
+var background = document.getElementById('projects');
+
+function activar(proyecto, elemento) {
+  // Reiniciar todos los "+" de los otros botones
+  document.querySelectorAll('.mas-text').forEach(span => span.textContent = '+');
+
+  // Cambiar el fondo según el proyecto
+  switch(proyecto) {
+    case 'capitalis':
+      background.style.background = "linear-gradient(90deg, hsla(138, 20%, 38%, 1) 0%, hsla(152, 65%, 53%, 1) 26%, hsla(152, 77%, 56%, 1) 43%, hsla(117, 54%, 72%, 1) 76%, hsla(144, 69%, 63%, 1) 93%)";
+      break;
+    case 'hcs':
+      background.style.background = "linear-gradient(90deg, hsla(334, 59%, 34%, 1) 0%, hsla(337, 93%, 45%, 1) 22%, hsla(333, 80%, 60%, 1) 45%, hsla(334, 100%, 72%, 1) 100%)";
+      break;
+    case 'lya':
+      background.style.background = "linear-gradient(90deg, hsla(70, 17%, 34%, 1) 0%, hsla(59, 42%, 68%, 1) 22%, hsla(27, 38%, 94%, 1) 52%, hsla(44, 41%, 89%, 1) 100%)";
+      break;
+  }
+
+  // Cambiar el "+" por "×" solo en el que clickeaste
+  const icono = elemento.querySelector('.mas-text');
+  icono.textContent = '×';
+}
